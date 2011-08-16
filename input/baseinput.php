d0aac267dcbd70cf68874a851f2130c32bba366a
marcel.bohlaender <marcel.bohlaender@cn-consult.de>
2011-08-16 16:06:18 +0200

    Add EPFGenericPlugin extension

--- input/baseinput.php
+++ input/baseinput.php
@@ -1,4 +1,6 @@
 <?php
+require_once("./src/epf/epfgenericplugin.php");
+
 /**
  *
  *
@@ -6,7 +8,7 @@
  * @copyright 2011
  */
 
-abstract class BaseInput
+abstract class BaseInput extends epfGenericPlugin
 {
 
 	abstract function setFilename($_filename);
